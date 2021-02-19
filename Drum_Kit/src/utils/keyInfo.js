8af39d67b0f2a1885cca73b1d27f64e1d135d31e
LeeJaeYun <ag502@naver.com>
2021-02-20 01:33:15 +0900

    fix - babel 플러그인 추가

---
+++ Drum_Kit/src/utils/keyInfo.js
@@ -0,0 +1,13 @@
+const keyInfo = [
+    {key: "1", type: "clap"},
+    {key: "2", type: "hihat"},
+    {key: "3", type: "kick"},
+    {key: "4", type: "openhat"},
+    {key: "5", type: "boom"},
+    {key: "6", type: "ride"},
+    {key: "7", type: "snare"},
+    {key: "8", type: "tom"},
+    {key: "9", type: "tink"}
+]
+
+export default keyInfo
