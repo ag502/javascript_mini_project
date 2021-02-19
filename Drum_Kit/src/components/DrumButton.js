ef45899c03461cc82001ecd24b9d602f4c897eff
LeeJaeYun <ag502@naver.com>
2021-02-20 01:17:09 +0900

    feature-drum_button 구현

---
+++ Drum_Kit/src/components/DrumButton.js
@@ -0,0 +1,26 @@
+class DrumButton {
+    constructor($target, {key, type}) {
+        this.key = key
+        this.type = type
+
+        this.$button = document.createElement("div")
+        this.$button.classList.add("key")
+
+        $target.appendChild(this.$button)
+        this.render()
+    }
+
+    render = () => {
+        const $keyName = document.createElement("kbd")
+        $keyName.textContent = this.key
+
+        const $keyType = document.createElement("span")
+        $keyType.classList.add("sound")
+        $keyType.textContent = this.type
+
+        this.$button.appendChild($keyName)
+        this.$button.appendChild($keyType)
+    }
+}
+
+export default DrumButton
---
+++ Drum_Kit/src/components/Pad.js
@@ -0,0 +1,13 @@
+import DrumButton from './DrumButton.js'
+
+class Pad {
+    constructor($target) {
+        this.render()
+    }
+
+    render = () => {
+
+    }
+}
+
+export default Pad
